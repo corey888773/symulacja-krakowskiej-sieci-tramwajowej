a23a897a9ac070761bcbfa772008c84a98f7defa
szymiyay <szymoon09@gmail.com>
2023-12-11 12:05:30 +0100

    Add distance_to method to Node class and update file paths in main.py

--- simulation/main.py
+++ simulation/main.py
@@ -1,12 +1,14 @@
 from simulation import Simulation
 import json
+import os
 
 
 def main() -> None:
-    with open("../datapreprocessing/data/physical_network.json", "r", encoding="utf8") as f:
+    curr_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(f"{curr_dir}/../datapreprocessing/data/physical_network.json", "r", encoding="utf8") as f:
         network_model_physical = json.load(f)
 
-    with open("../datapreprocessing/data/logical_network.json", "r", encoding="utf8") as f:
+    with open(f"{curr_dir}/../datapreprocessing/data/logical_network.json", "r", encoding="utf8") as f:
         network_model_logical = json.load(f)
 
     simulation = Simulation(network_model_logical, network_model_physical)
--- simulation/models/node.py
+++ simulation/models/node.py
@@ -1,8 +1,13 @@
+import math
+
 class Node:
     def __init__(self, id: int, x: float, y: float, ) -> None:
         self.id = id
         self.x = x
         self.y = y
+
+    def distance_to(self, other) -> float:
+        return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
 
     def to_json(self) -> dict:
         return {
